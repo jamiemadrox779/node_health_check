55b54e87e69f21abc721483ab3e80047b0266be8
Ubuntu <ubuntu@ip-172-31-18-165.us-east-2.compute.internal>
2021-02-17 11:28:54 +0000

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * Sovryn node health check
+*/
+const express= require('express');
+const app = express();
+const http = require('http').createServer(app);
+const Web3 = require('web3');
+     
+const serverPort = 3001;
+const web3 = new Web3('https://mainnet2.sovryn.app/rpc');
+const web3S = new Web3('wss://mainnet2.sovryn.app/ws');
+const web3Iov = new Web3('https://public-node.rsk.co');
+
+
+http.listen(serverPort, () => {
+    console.log('listening on *:'+serverPort);
+});
+
+app.get('/', async (req, res)=> {
+    const b = await web3.eth.getBlockNumber();
+    const s = await web3S.eth.getBlockNumber();
+    const i = await web3Iov.eth.getBlockNumber();
+    console.log(new Date(Date.now())+ " processed blocks: rpc "+b+", wss: "+s+"  iov: "+i);
+
+    if(b===s && b===i) return res.status(200).send("ok");
+    res.status(500).send("not in sync");
+});
