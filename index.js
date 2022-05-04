51c2c5987a986a01bc3f074c38fb448192d5fb89
Ubuntu <ubuntu@ip-172-31-17-144.us-east-2.compute.internal>
2022-05-04 18:37:51 +0000

    new healthcheck

--- index.js
+++ index.js
@@ -10,27 +10,36 @@ const serverPort = 3001;
 let web3;
 let web3S;
 let web3Iov;
-// wait for rsk node to start
-setTimeout(startListening, 20000);
 
-function startListening(){
-   web3 = new Web3("http://127.0.0.1:4444");
-   web3S = new Web3("ws://127.0.0.1:4445/websocket");
-   web3Iov = new Web3('https://public-node.rsk.co');
 
-   http.listen(serverPort, () => {
-       console.log('listening on *:'+serverPort);
+http.listen(serverPort, () => {
+    console.log('listening on *:'+serverPort);
    });
-}
+
 app.get('/', async (req, res)=> {
-    const b = await web3.eth.getBlockNumber();
-    const s = await web3S.eth.getBlockNumber();
-    const i = await web3Iov.eth.getBlockNumber();
 
-    const result = new Date(Date.now())+ " processed blocks: rpc "+b+", wss: "+s+"  iov: "+i;
-    console.log(result);
-
-    if(Math.abs(b - s) <= 3 && Math.abs(b-i)<=3) return res.status(200).send(result);
-
-    return res.status(503).send("not in sync - " + result);
+    try{
+        web3 = new Web3("http://127.0.0.1:4444");
+        web3S = new Web3("ws://127.0.0.1:4445/websocket");
+        web3Iov = new Web3('https://public-node.rsk.co');
+        const rpc = await web3.eth.getBlockNumber();
+        const ws = await web3S.eth.getBlockNumber();
+        const iov = await web3Iov.eth.getBlockNumber();
+        const account = await web3.eth.accounts.create();  
+        const rpcQuery1 = await web3.eth.getBalance((account.address).toString(),'latest');
+        const rpcQuery2 = await web3.eth.getCode((account.address).toString(), "latest");
+        const syncResult = new Date(Date.now())+ " processed blocks: rpc "+rpc+", wss: "+ws+"  iov: "+iov+" getBalance:"+rpcQuery1+" getCode:"+rpcQuery2+"\n";
+        if(Math.abs(rpc - ws) <= 3 && Math.abs(iov - rpc) <=3 ) {
+            return res.status(200).send(syncResult);
+          } 
+        else if(rpc > iov ) {
+            return res.status(200).send(syncResult);
+          }    
+        else {
+            return res.status(503).send("not in sync - " + syncResult);
+        } 
+     }  
+    catch(err){
+        return res.status(503).send("rsk node not responding: " + err + "\n");
+    }
 });
